fae8853e7a20705941493628ae79c5b418dd73d2
Lucas Vieira <contatolucas@gmail.com>
2021-01-20 16:03:14 -0300

    Commit inicial

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 
 from aws_cdk import core
-
-from aws_cdk_bootcamp_teste.aws_cdk_bootcamp_teste_stack import AwsCdkBootcampTesteStack
+from s3_stack.s3 import S3Stack
 
 
 app = core.App()
-AwsCdkBootcampTesteStack(app, "aws-cdk-bootcamp-teste")
-
+S3Stack(app, 'stack-cdk-bucket')
 app.synth()
